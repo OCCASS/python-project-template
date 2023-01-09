fc2b916f35cf1a61fb57c52b0525957ee87d093d
Timur Pavlov <72919856+OCCCAS@users.noreply.github.com>
2023-01-09 21:51:23 +0300

    fix: __main__.py fix

--- src/__main__.py
+++ src/__main__.py
@@ -1,17 +1,1 @@
-import uvicorn
-from fastapi import FastAPI
 
-from src.models import PinnacleEvent
-from src.pinnacle import Pinnacle
-
-app = FastAPI()
-
-
-@app.post('/fetch_cart/')
-async def handle_fetch_cart(event: PinnacleEvent):
-    pinnacle = Pinnacle()
-    return await pinnacle.fetch_odds(event)
-
-
-if __name__ == '__main__':
-    uvicorn.run(app, host="127.0.0.1", port=3030)
